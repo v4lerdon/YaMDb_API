fafa97fcb317825e8b59239aa2e505f0ca9d114c
Michail Baskov <97636962+Straga33@users.noreply.github.com>
2022-06-03 18:09:14 +0300

    Update models.py
    добавлена модель юзера

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
+)
 
-# Create your models here.
+
+ADMIN_ROLE = [
+    ('user', 'user'),
+    ('admin', 'admin'),
+    ('moderator', 'moderator'),
+]
+
+
+class UserManager(BaseUserManager):
+    """
+    Django требует, чтобы кастомные пользователи определяли свой собственный
+    класс Manager. Унаследовавшись от BaseUserManager, мы получаем много того
+    же самого кода, который Django использовал для создания User (для демонстрации).
+    """
+
+    def create_user(self, username, email, password=None):
+        """ Создает и возвращает пользователя с имэйлом, паролем и именем. """
+        if username is None:
+            raise TypeError('Необходимо указать имя пользователя.')
+
+        if email is None:
+            raise TypeError('Необходимо указать email пользователя.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """ Создает и возввращет пользователя с привилегиями суперадмина. """
+        if password is None:
+            raise TypeError('Необходимо задать пароль суперпользователю.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)    
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(max_length=15,choices=ADMIN_ROLE,default='user')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = UserManager()
+
+    def __str__(self):
+        """ Строковое представление модели (отображается в консоли) """
+        return self.email

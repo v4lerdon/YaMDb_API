917edc3402d75458739d5ae1d89435bd70cee46e
Michail Baskov <97636962+Straga33@users.noreply.github.com>
2022-06-05 03:12:54 +0300

    Update views.py
    реализованы регистрация нового пользователя и получение токена

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,30 +1,51 @@
-from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import AccessToken
 
-from .serializers import UserSerializer
+from reviews.models import User
 
+from api.serializers import UserSerializer, TokenSerializer
 
-class CreateUserAPIView(APIView):
-    """
-    Разрешить всем пользователям (аутентифицированным и нет) доступ к данному эндпоинту.
-    """
+#регистрация нового пользователя
+class UserSignupViewset(APIView):
     permission_classes = (AllowAny,)
 
-    @csrf_exempt
-    @action(detail=True, methods=['post'])
-    def post(self, request):
-        user = request.data
-        serializer = UserSerializer(data=user)
+    def post(self, request):        
+        serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()        
-        #создаем токен  и отпраляем его на почту
-        #confirmation_code = default_token_generator.make_token(user)
-        send_mail('Token api_yamdb', 'your token:', 'admin@yamdb.ru', ['baem-festa@yandex.ru'])
+        if request.data['username'] == 'me':
+            return Response(
+                {'Использовать имя "me" в качестве username запрещено.'}, status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer.save()
+        user = get_object_or_404(User, username=request.data['username'])
+        token = default_token_generator.make_token(user)
+        send_mail(
+                'confirmation_code',
+                token,
+                'admin@yamdb.ru',
+                [serializer.validated_data['email']]
+        )
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+#получение токена по confirmation_code
+class UserTokenViewset(APIView):
+    permission_classes = (AllowAny,)   
+
+    def post(self, request):
+        serializer = TokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = get_object_or_404(User, username=request.data['username'])
+
+        if default_token_generator.check_token(user, request.data['confirmation_code']):
+            token = AccessToken.for_user(user)
+            return Response(
+                {'token': str(token)}, status=status.HTTP_200_OK
+            )
+        return Response({'Не удалось сформировать токен'}, status=status.HTTP_400_BAD_REQUEST)
+    

dad6cfabb7f9960e433ee07ba3b21ad9bdc043f2
Валерий Юрченко <valerayurchenko14@yandex.ru>
2022-06-05 21:25:43 +0500

    del user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.db import models
 from django.contrib.auth import get_user_model
 
-# User = get_user_model()
-
 
 class Category(models.Model):
     title = models.CharField(max_length=100)

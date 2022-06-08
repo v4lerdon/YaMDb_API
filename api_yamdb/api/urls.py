16db3de10327f7d0bc17e29c62318befeab4c1c7
Michail Baskov <97636962+Straga33@users.noreply.github.com>
2022-06-08 19:46:38 +0300

    Update urls.py
    добавлены urls для работы с редактированием и добавление user  и работы со своим профилем

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from api.views import UserSignupViewset, UserTokenViewset
-
+from api.views import (UserMeRetrieveUpdate, UserSignupViewset,
+                       UsersSettingsViewset, UserTokenViewset)
 
 app_name = 'api'
 
+router = DefaultRouter()
+
+router.register(r'users', UsersSettingsViewset, basename='usersettings')
+
 urlpatterns = [
     path('v1/auth/signup/', UserSignupViewset.as_view()),
-    path('v1/auth/token/', UserTokenViewset.as_view())
+    path('v1/auth/token/', UserTokenViewset.as_view()),
+    path('v1/users/me/', UserMeRetrieveUpdate.as_view()),
+    path('v1/', include(router.urls)),
 ]

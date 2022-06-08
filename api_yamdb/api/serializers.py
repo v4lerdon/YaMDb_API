66bfcc4582858ec39fba93190b07f7c3fb852bd0
Michail Baskov <97636962+Straga33@users.noreply.github.com>
2022-06-08 19:48:31 +0300

    Update serializers.py
    добавлены сериализаторы для работы с учетной записью, изменения / создания пользователя администратором

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,16 +2,60 @@ from rest_framework import serializers
 
 from reviews.models import User
 
-#селитизатор модели user при регистрации пользователя
-class UserSerializer(serializers.ModelSerializer):
-    """ Сериализация регистрации пользователя и создания нового. """
 
+class UserSignupSerializer(serializers.ModelSerializer):
+    """ Сериализация регистрации пользователя и создания нового. """
     class Meta:
         model = User
         fields = ['email', 'username']
 
-        
-#селитизатор для токена
 class TokenSerializer(serializers.Serializer):
+    """Сериализация токена."""
     username = serializers.CharField(max_length=255)
     confirmation_code = serializers.CharField()
+
+
+class UsersSettingsSerializer(serializers.ModelSerializer):
+    """Сериализация изменение/создание user администратором."""
+    first_name = serializers.CharField(
+        required=False, max_length=150, allow_null=True
+    )
+    last_name = serializers.CharField(
+        required=False, max_length=150, allow_null=True
+    )
+    role = serializers.ChoiceField(
+        choices=['user', 'moderator', 'admin'], default='user'
+    )
+    class Meta:
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        ]
+        model = User
+
+
+class UserMeSerializer(serializers.ModelSerializer):
+    """Сериализация запроса/измения своей учетной записи."""
+    username = serializers.CharField(required=False)
+    email = serializers.EmailField(required=False)
+    first_name = serializers.CharField(
+        required=False, max_length=150, allow_null=True
+    )
+    last_name = serializers.CharField(
+        required=False, max_length=150, allow_null=True
+    )
+    role = serializers.CharField(read_only=True)
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        ]
